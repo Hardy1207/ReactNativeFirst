ea510ccffd3db4d9d16c9b8f8ce1ad8b9d86ebf2
Meer <p.meer@meerp-a.itransition.corp>
2019-01-18 17:24:23 +0300

    first commit

---
+++ server/app.js
@@ -0,0 +1,34 @@
+var express =require('express');
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+  
+// установка схемы
+const userScheme = new Schema({
+    name: String,
+    age: Number
+});
+  
+// подключение
+mongoose.connect("mongodb://localhost:27017/TaskDB", { useNewUrlParser: true });
+  
+const User = mongoose.model("User", userScheme);
+const user = new User({
+    name: "Bill",
+    age: 41
+});
+  
+user.save(function(err){
+    mongoose.disconnect();  // отключение от базы данных
+      
+    if(err) return console.log(err);
+    console.log("Сохранен объект", user);
+});
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
